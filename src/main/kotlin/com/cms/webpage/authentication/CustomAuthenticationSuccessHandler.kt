d89e7ffc1b6bebbbc736856813b0e1610708c6ad
Krancki <kr.przygocki@gmail.com>
2020-01-19 17:12:51 +0100

    test

---
+++ src/main/kotlin/com/cms/webpage/authentication/CustomAuthenticationSuccessHandler.kt
@@ -0,0 +1,14 @@
+package com.cms.webpage.authentication
+
+import org.springframework.security.core.Authentication
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler
+import org.springframework.stereotype.Component
+import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletResponse
+
+@Component
+class CustomAuthenticationSuccessHandler: AuthenticationSuccessHandler{
+    override fun onAuthenticationSuccess(p0: HttpServletRequest?, p1: HttpServletResponse?, p2: Authentication?) {
+        p1?.status = 200
+    }
+}
